3c8c8b85a63d7f469aebfd6a7b6fc35696077235
kronicmnemonik <36289713+kronicmnemonik@users.noreply.github.com>
2025-02-27 16:24:08 -0500

    Update ProductService.java

--- src/main/java/com/tradebeyond/microservice/service/ProductService.java
+++ src/main/java/com/tradebeyond/microservice/service/ProductService.java
@@ -1,7 +1,7 @@
 package com.tradebeyond.microservice.service;
 
-import com.example.demo.model.Product;
-import com.example.demo.repository.ProductRepository;
+import com.tradebeyond.microservice.model.Product;
+import com.tradebeyond.microservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
